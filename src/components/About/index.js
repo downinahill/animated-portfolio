1bcace012dad207a8a6fcb2f3b43bdea129ae305
downinahill <brian.hill.0023@gmail.com>
2022-05-03 08:34:04 -0400

    added about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+// import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+// import { faPython, faCss3, faGitAlt, faHtml5, faJsSquare, faReact} from '@fortawesome/free-brands-svg'
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        // return setTimeout(() => {
+
+            setLetterClass('text-animate-hover')
+        }, [])
+    // }, [])
+
   return (
     <div className='container about-page'>
         <div className='text-zone'>
             <h1>
-                <AnimatedLetters />
+                <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                    idx={15}
+                />
             </h1>
+            <p>I'm ambitious.</p>
+            <p>I'm confident.</p>
+            <p>I'm creative.</p>
+
+
         </div>
+
+      
+
     </div>
   )
 }
