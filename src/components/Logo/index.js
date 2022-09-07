f73723e4befeeed59f767cf2b5f22f8fe65881dc
downinahill <brian.hill.0023@gmail.com>
2022-09-07 17:26:01 -0400

    removed GSAP

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef }  from 'react'
+import React, { useRef }  from 'react'
 import './index.scss';
 import LogoB from '../../assets/images/B-Letter-PNG.png';
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
+
 
 const Logo = () => {
 
@@ -10,32 +9,7 @@ const Logo = () => {
     const outlineLogoRef = useRef();
     const solidLogoRef = useRef();
 
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
 
-        gsap.timeline()
-        .to(bgRef.current, {
-            duration: 1,
-            opacity: 1
-        })
-        .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20,
-        })
-
-        gsap.fromTo(
-            solidLogoRef.current,
-            {
-                opacity: 0,
-
-            },
-            {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
-    }, [])
 
     return (
         <div className='logo-container' ref={bgRef}>
